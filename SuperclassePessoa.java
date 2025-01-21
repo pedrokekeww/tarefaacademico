f98b3e24de9abf29fbf61b43a3b2ef300247a5be
pedrokekeww <pedroveb119@gmail.com>
2025-01-21 13:51:36 -0300

    Create SuperclassePessoa.java

---
+++ SuperclassePessoa.java
@@ -0,0 +1,41 @@
+//superclasse pessoa no sistema academico.
+package academico;
+
+public class pessoa 
+{
+	private String nome;
+	private String email;
+	private double idade;
+	private String sobrenome;
+	private String sexo;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getIdade() {
+		return idade;
+	}
+	public void setIdade(double idade) {
+		this.idade = idade;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}
